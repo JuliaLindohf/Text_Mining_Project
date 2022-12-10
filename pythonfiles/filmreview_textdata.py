097e1c4385ca64bcbb78c1baa988043e1592c80c
Julia Lindohf <55234903+JuliaLindohf@users.noreply.github.com>
2022-12-10 03:38:49 +0100

    Update filmreview_textdata.py

--- pythonfiles/filmreview_textdata.py
+++ pythonfiles/filmreview_textdata.py
@@ -8,8 +8,10 @@ nltk.download('punkt')
 import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 
+
 !wget http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Movies_and_TV_5.json.gz
 !gzip -d reviews_Movies_and_TV_5.json.gz
+
 
 f = open("reviews_Movies_and_TV_5.json", "r")
 filmData = [json.loads(line) for line in f.readlines()]
