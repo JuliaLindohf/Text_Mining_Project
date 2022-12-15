3df53d6428a3554f7db9f5862c4a4de488445457
Julia Lindohf <55234903+JuliaLindohf@users.noreply.github.com>
2022-12-15 01:08:14 +0100

    Update kmeanscluster.py

--- pythonfiles/kmeanscluster.py
+++ pythonfiles/kmeanscluster.py
@@ -5,10 +5,11 @@ import numpy as np
 import json
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
-nltk.download('punkt')
+nltk.download('all')
 import spacy
 from nltk.stem import WordNetLemmatizer
 import nltk
 from nltk.corpus import stopwords
 from gensim.models import Word2Vec
 from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import cosine_similarity
